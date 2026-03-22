3c4573defaf9796a11c60033d2c9648cae831848
Eugenio <aboudahercompanybr@gmail.com>
2026-03-22 16:17:11 -0300

    cell phone login 22 - novo chat

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 const { login } = require('../controllers/auth.controller');
 
-router.post('/login', login);
+router.post('/login', (req, res, next) => {
+  console.log("LOGIN SESSION ANTES:", req.session);
+  next();
+}, login);
 
 module.exports = router;
