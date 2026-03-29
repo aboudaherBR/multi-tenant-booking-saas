67fc5ca6c8405d325613fe7e1fdae3d09d60f032
Eugenio <aboudahercompanybr@gmail.com>
2026-03-29 00:20:18 -0300

    Arrumou a page do profissional problema tela admin  CODEX1 10

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,7 +1,7 @@
 
 import { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import { useAuth } from '../../hooks/AuthContext.jsx';
+import { useAuth } from '../hooks/AuthContext';
 
 function LoginPage() {
   const { login, isAuthenticated, user, loading } = useAuth();
