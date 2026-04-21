22aa31e8d2b5af78fc39c77f3ff5117d6b49588b
Eugenio <aboudahercompanybr@gmail.com>
2026-04-21 09:51:48 -0300

    feat: add GET route for client appointments lookup (query param phone) - modal appontments per client 21

--- frontend/src/components/AppointmentsModal.jsx
+++ frontend/src/components/AppointmentsModal.jsx
@@ -29,8 +29,7 @@ export default function AppointmentsModal({ appointments, onClose }) {
                                         style={cardStyle}
                                     >
                                         <strong style={{ fontSize: "16px" }}>
-                                            {console.log("DATE STRING:", a.date)}
-                                            {formatDateBRSafe(a.date, a.start_time)}
+                                            {formatDateBR(a.date, a.start_time)}
                                         </strong>
 
                                         <div style={metaStyle}>
