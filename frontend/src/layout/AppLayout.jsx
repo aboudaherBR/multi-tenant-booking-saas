dd22739b096932fe79f721352b7e026ee5940f82
Eugenio <aboudahercompanybr@gmail.com>
2026-04-23 11:26:49 -0300

    feat/ style se schedule  page 25

--- frontend/src/layout/AppLayout.jsx
+++ frontend/src/layout/AppLayout.jsx
@@ -1,7 +1,8 @@
-import styles from './AppLayout.module.css';
+import styles from './AppLayout.css';
 import { Settings } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 import logo from "../assets/logo_png.png";
+
 
 function AppLayout({ children }) {
   const navigate = useNavigate();
