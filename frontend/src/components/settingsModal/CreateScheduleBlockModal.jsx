b12601847af79efe81eabcf105548b850455e425
Eugenio <aboudahercompanybr@gmail.com>
2026-05-02 12:10:49 -0300

    feat/ style settings page - blocked hours modal 12

--- frontend/src/components/settingsModal/CreateScheduleBlockModal.jsx
+++ frontend/src/components/settingsModal/CreateScheduleBlockModal.jsx
@@ -1,29 +1,36 @@
+import { useState } from "react";
+
+
 export default function CreateScheduleBlockModal({
-  isOpen,
-  onClose
+    isOpen,
+    onClose
 }) {
-  if (!isOpen) return null;
+    const [startDateTime, setStartDateTime] = useState("");
+    const [endDateTime, setEndDateTime] = useState("");
+    if (!isOpen) return null;
 
-  return (
-    <div className="modal-backdrop">
-      <div className="modal-content">
+    
 
-        <div className="modal-header">
-          <h3 className="heading">Novo bloqueio</h3>
+    return (
+        <div className="modal-backdrop">
+            <div className="modal-content">
 
-          <button
-            className="button-icon"
-            onClick={onClose}
-          >
-            ✕
-          </button>
+                <div className="modal-header">
+                    <h3 className="heading">Novo bloqueio</h3>
+
+                    <button
+                        className="button-icon"
+                        onClick={onClose}
+                    >
+                        ✕
+                    </button>
+                </div>
+
+                <p className="text-muted">
+                    Aqui você criará um novo bloqueio.
+                </p>
+
+            </div>
         </div>
-
-        <p className="text-muted">
-          Aqui você criará um novo bloqueio.
-        </p>
-
-      </div>
-    </div>
-  );
+    );
 }
