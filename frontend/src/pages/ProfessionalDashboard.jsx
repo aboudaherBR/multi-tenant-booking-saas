fe63dbb514bf694013dc1df4d17607c461c1a2ef
Eugenio <aboudahercompanybr@gmail.com>
2026-05-15 14:57:08 -0300

    feat/professionals dashboard 9

--- frontend/src/pages/ProfessionalDashboard.jsx
+++ frontend/src/pages/ProfessionalDashboard.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import api from "../api/apiClient";
 import { useParams } from "react-router-dom";
+import "../../src/global.css";
 
 export default function ProfessionalDashboard() {
 
