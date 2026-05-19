bf83a19a5bc73477f8f14ce80c5a5e5da99e7db8
Eugenio <aboudahercompanybr@gmail.com>
2026-05-19 09:28:20 -0300

    feat/professionals dashboard - reports 5

--- frontend/src/pages/ProfessionalReportsPage.jsx
+++ frontend/src/pages/ProfessionalReportsPage.jsx
@@ -1,46 +1,102 @@
 import "../../src/global.css";
+import { useEffect, useState } from "react";
+import api from "../api/apiClient";
+import { useParams } from "react-router-dom";
 
 export default function ProfessionalReportsPage() {
 
-  return (
 
-    <div
-      style={{
-        minHeight: "100vh",
-        background: "var(--color-bg)"
-      }}
-    >
+    const [report, setReport] =
+        useState(null);
 
-      <div className="header-gradient">
+    const {
+        companySlug,
+        professionalSlug
+    } = useParams();
 
-        <h2 style={{ color: "white" }}>
-          Relatórios
-        </h2>
+    async function fetchReport() {
 
-      </div>
+        try {
 
-      <div
-        className="container-main"
-        style={{
-          marginTop: "-40px"
-        }}
-      >
+            const today = new Date();
+
+            const startDate =
+                new Date(
+                    today.getFullYear(),
+                    today.getMonth(),
+                    1
+                )
+                    .toISOString()
+                    .split("T")[0];
+
+            const endDate =
+                new Date(
+                    today.getFullYear(),
+                    today.getMonth() + 1,
+                    0
+                )
+                    .toISOString()
+                    .split("T")[0];
+
+            const data = await api(
+                `/public/${companySlug}/${professionalSlug}/report?startDate=${startDate}&endDate=${endDate}`
+            );
+
+            setReport(data);
+
+        } catch (error) {
+
+            console.error(
+                "Erro ao buscar relatório:",
+                error
+            );
+        }
+    }
+
+    useEffect(() => {
+        fetchReport();
+    }, []);
+
+
+    return (
 
         <div
-          className="card"
-          style={{
-            padding: "20px"
-          }}
+            style={{
+                minHeight: "100vh",
+                background: "var(--color-bg)"
+            }}
         >
 
-          <h1 className="heading">
-            Relatórios do profissional
-          </h1>
+            <div className="header-gradient">
+
+                <h2 style={{ color: "white" }}>
+                    Relatórios
+                </h2>
+
+            </div>
+
+            <div
+                className="container-main"
+                style={{
+                    marginTop: "-40px"
+                }}
+            >
+
+                <div
+                    className="card"
+                    style={{
+                        padding: "20px"
+                    }}
+                >
+
+                    <h1 className="heading">
+                        Relatórios do profissional
+                    </h1>
+
+                </div>
+
+            </div>
 
         </div>
-
-      </div>
-
-    </div>
-  );
+    );
 }
