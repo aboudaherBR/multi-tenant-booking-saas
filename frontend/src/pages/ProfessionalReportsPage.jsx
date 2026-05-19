ecb3f5e21e63e7e9cfaa59a442a263c99105d7c8
Eugenio <aboudahercompanybr@gmail.com>
2026-05-19 10:48:09 -0300

    feat/professionals dashboard - reports 22

--- frontend/src/pages/ProfessionalReportsPage.jsx
+++ frontend/src/pages/ProfessionalReportsPage.jsx
@@ -1,7 +1,10 @@
 import "../../src/global.css";
 import { useEffect, useState } from "react";
 import api from "../api/apiClient";
-import { useParams } from "react-router-dom";
+import {
+  useParams,
+  useNavigate
+} from "react-router-dom";
 
 export default function ProfessionalReportsPage() {
 
