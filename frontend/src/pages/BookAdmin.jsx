4e445278e120ee77077a7a0c6dff3eee7492fd21
Eugenio <aboudahercompanybr@gmail.com>
2026-05-22 10:49:09 -0300

    feat/ admin booking -- existing client -- confirm appointment 2

--- frontend/src/pages/BookAdmin.jsx
+++ frontend/src/pages/BookAdmin.jsx
@@ -293,6 +293,7 @@ export default function BookAdmin() {
                         onConfirm={() => {
 
                             onConfirm={handleConfirmBooking}
+                            console.log("CONFIRMADO!");
 
                         }}
                     />
